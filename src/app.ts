4b329eb4b145c14fa3f780e62db2a520035da76f
JaViKo500 <javikogutierrez64@gmail.com>
2024-05-24 14:06:30 -0500

    server with web sockets

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -8,8 +8,27 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    const payload = {
+      type: 'custom-message',
+      payload: data?.toString().toUpperCase(),
+    };
+    // ws.send(JSON.stringify(payload));
+
+    wss.clients.forEach(function each(client) {
+
+      // * all
+      // if ( client.readyState === WebSocket.OPEN ) {
+      //   client.send( JSON.stringify(payload), { binary: false } );
+      // }
+      if ( client !== ws &&  client.readyState === WebSocket.OPEN ) {
+        client.send( JSON.stringify(payload), { binary: false } );
+      }
+    });
   });
 
   ws.send('hello from server');
+
+  ws.on( 'close', () => {
+    console.log('client disconnected');
+  });
 });
